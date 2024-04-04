c1aea2b46654a2e062ed1cade325e3587e63896d
iFelka <mister.ti2014@gmail.com>
2024-04-04 19:09:14 +0700

    Create treugol'nik.cpp

--- treugol'nik.cpp
+++ treugol'nik.cpp
@@ -3,29 +3,29 @@
 using namespace std;
 
 void Perimetr(float a, float b, float c) {
-    float P = a + b + c;//Периметр
-    cout << "Периметр треугольника равен " << P << endl;
+    float P = a + b + c;//��������
+    cout << "�������� ������������ ����� " << P << endl;
 }
 
 void PloshadGERONa(float a, float b, float c) {
-    float p = (a + b + c) / 2;//Преметр деленый на 2
-    float S = sqrt(p * (p - a) * (p - b) * (p - c));//Площадь треугольника
-    cout << "Площадь треугольника равна " << S << endl;
+    float p = (a + b + c) / 2;//������� ������� �� 2
+    float S = sqrt(p * (p - a) * (p - b) * (p - c));//������� ������������
+    cout << "������� ������������ ����� " << S << endl;
 }
 
 void Ravnobedrennost(float a, float b, float c) {
-    if (a == b || b == c || a == c) {//Проверка на равнобедренность
-        cout << "Треугольник является равнобедренным" << endl;
+    if (a == b || b == c || a == c) {//�������� �� ����������������
+        cout << "����������� �������� ��������������" << endl;
     }
     else {
-        cout << "Треугольник не является равнобедренным" << endl;
+        cout << "����������� �� �������� ��������������" << endl;
     }
 }
 
 int main() {
     setlocale(LC_ALL, "RU");
     float a, b, c;
-    cout << "Введите 3 стороны треугольника:" << endl;
+    cout << "������� 3 ������� ������������:" << endl;
     cin >> a >> b >> c;
     Perimetr(a, b, c);
     PloshadGERONa(a, b, c);
